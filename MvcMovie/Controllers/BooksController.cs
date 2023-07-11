c3807c41d9e6a558926b7f3ddea0877b2dd9ca82
iworsham <15iworsham@gmail.com>
2023-07-11 12:34:05 -0400

    committ

---
+++ MvcMovie/Controllers/BooksController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class BooksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Cart()
+        {
+            return View();
+        }
+        public IActionResult Returns()
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcMovie/Controllers/MoviesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcMovie.DataAccess;
+
+namespace MvcMovie.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // for every controller that accesses a database, we need a context property.
+        private readonly MvcMovieContext _context;
+
+        // We need to create that context when we create the controller.
+        public MoviesController(MvcMovieContext context)
+        {
+            _context = context;
+        }
+        // controller methods are called Actions
+        // GET "/movies" <- this route is determined by the name of the controller
+        public IActionResult Index()
+        {
+            var movies = _context.Movies;
+            return View(movies); //.NET will look for the view called Movies/Index
+        }
+        public IActionResult Cart()
+        {
+            return View();
+        }
+    }
+}
